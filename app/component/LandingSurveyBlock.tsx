258a3b46649cd5638f507d3e08fdcd0ffbd07a22
sidjey-y <haboc.carlajoy@gmail.com>
2026-02-25 10:04:50 +0800

    deploy

--- app/component/LandingSurveyBlock.tsx
+++ app/component/LandingSurveyBlock.tsx
@@ -107,6 +107,8 @@ export default function LandingSurveyBlock({ onContinue }: LandingSurveyBlockPro
     return (
       <div
         style={{
+          position: "absolute",
+          inset: 0,
           width: "100%",
           height: "100%",
           display: "flex",
--- app/landing-page/page.tsx
+++ app/landing-page/page.tsx
@@ -109,6 +109,8 @@ export default function LandingSurveyBlock({ onContinue }: LandingSurveyBlockPro
     return (
       <div
         style={{
+          position: "absolute",
+          inset: 0,
           width: "100%",
           height: "100%",
           display: "flex",
